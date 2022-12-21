592b3e08d82a2ba594277eec5cda206be35b7991
imran <hafizimranattari@gmail.com>
2022-12-21 10:46:04 +0500

    hellow 005

--- server.mjs
+++ server.mjs
@@ -1,6 +1,10 @@
-import express from 'express'
-const app = express()
-const port = 3003
+import express,{ urlencoded } from 'express'; 
+ import  cors from 'cors';
+ import  dotenv from 'dotenv';  
+ dotenv.config();
+ const app = express();
+
+const port=process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send(res.ip+ ' Hello   World! ' +req.ip)
